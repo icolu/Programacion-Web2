f5f00958c0d9524a02ccacdbc3043ed52cacd10b
icolu <icoluccio@itba.edu.ar>
2022-11-13 20:24:24 -0300

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -1,15 +1,11 @@
 function agarrar_precio(url){
-var req = new XMLHttpRequest();
-req.overrideMimeType("application/json");
-req.open('GET', url, true);
-req.onload  = function() {
-    var precio = document.getElementsByClassName("andes-money-amount__fraction")[0].textContent
-    console.log('$'+ precio)
-    var precio_final= '$'+ precio
-    return precio_final
-    };
-return precio_final
+    var req = new XMLHttpRequest();
+    req.overrideMimeType("application/json");
+    req.open('GET',url, true);
+    function valor() {
+        var precio = document.getElementsByClassName("andes-money-amount__fraction")[0].textContent
+        var precio_final= '$'+ precio
+        return precio_final
+    };  
+    return req.onload=valor()
 }
-
-
-
